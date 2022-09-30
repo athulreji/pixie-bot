fe55fc38a65326a7f698c0f9672038c6c5bf1200
adamsyy <adamoommenjacob@gmail.com>
2022-10-01 01:15:06 +0530

    test

--- main.py
+++ main.py
@@ -19,6 +19,16 @@ class MyClient(discord.Client):
             await guild.system_channel.send(to_send)
 
 
+#  code for the bot to send a hi when someone says hi       
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
+
+        if message.content.startswith('hi'):
+            await message.channel.send('Hello!')
+            
+
+
 intents = discord.Intents.default()
 intents.members = True
 
