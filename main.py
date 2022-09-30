f4f8f2cea7e9415bd2ae111c6cebd757efd1835c
Athul Reji <athulreji007@gmail.com>
2022-10-01 01:42:53 +0530

    hello

--- main.py
+++ main.py
@@ -27,16 +27,6 @@ class MyClient(discord.Client):
 
 
 
-#  code for the bot to send a hi when someone says hi       
-    async def on_message(self, message):
-        if message.author == client.user:
-            return
-
-        if message.content.startswith('hi'):
-            await message.channel.send('Hello!')
-            
-
-
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
